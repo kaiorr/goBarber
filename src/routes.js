a1fbe7feb558a9c2b57be23bed18ccf2906bd14b
kaiorr <kaio.rocharibeiro@gmail.com>
2020-02-28 07:29:40 -0300

    adicionado rotas de files

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middleware/auth';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 
 const routes = new Router();
@@ -15,8 +17,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json(req.file);
-});
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
