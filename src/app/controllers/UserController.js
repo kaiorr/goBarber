a91edd3ef88020dda81d14f38ec4f1a625802eb3
kaiorr <kaio.rocharibeiro@gmail.com>
2020-02-28 07:26:45 -0300

    Alteração de modo a varrer qualquer erro de sintaxe

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -10,12 +10,15 @@ class UserController {
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validação Incorreta' });
-  }
-    const userExists = await User.findOne({ where: { email: req.body.email } });
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
+
+    const userExists = await User.findOne({
+      where: { email: req.body.email }
+    });
 
     if (userExists) {
-      return res.status(400).json({ error: 'Usuário já exsite, email já cadastrado' });
+      return res.status(400).json({ error: 'Usuário já Cadastrado' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
@@ -25,7 +28,7 @@ class UserController {
       name,
       email,
       provider,
-    });
+  });
   }
 
   async update(req, res) {
@@ -35,33 +38,30 @@ class UserController {
       oldPassword: Yup.string().min(6),
       password: Yup.string().min(6).when('oldPassword', (oldPassword, field) =>
         oldPassword ? field.required() : field
-    ),
+      ),
       confirmPassword: Yup.string().when('password', (password, field) =>
         password ? field.required().oneOf([Yup.ref('password')]) : field
       ),
     });
 
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validação Incorreta' });
-    }
-
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
 
-    if (email && email !== user.email) {
+    if (email && email !== user.email){
       const userExists = await User.findOne({ where: { email } });
 
+
       if (userExists) {
-        return res.status(400).json({ error: 'Usuário já exsite, email já cadastrado' });
+        return res.status(401).json({ error: 'User already exists' });
       }
     }
 
-    if (oldPassword && !(await user.checkPassword(oldPassword))){
-      return res.status(401).json({ error: 'Senha Incorrreta' });
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body );
+    const { id, name, provider } = await user.update(req.body);
 
     return res.json({
       id,
@@ -71,4 +71,5 @@ class UserController {
     });
   }
 }
+
 export default new UserController();
