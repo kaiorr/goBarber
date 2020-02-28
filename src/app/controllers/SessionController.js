d3d49c405ec1d38c4e9e1796af36dd5928d3b01d
kaiorr <kaio.rocharibeiro@gmail.com>
2020-02-28 07:26:23 -0300

    Alteração de modo a varrer qualquer erro de sintaxe

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,14 +3,16 @@ import * as Yup from 'yup';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
+
 class SessionController {
   async store(req, res) {
     const schema = Yup.object().shape({
       email: Yup.string().email().required(),
-      password: Yup.string().required().min(6),
+      password: Yup.string().required(),
     });
-    if (!(await schema.isValid(req.body))){
-      return res.status(400).json({ error: 'Validação Incorreta' });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
     }
 
     const { email, password } = req.body;
@@ -18,7 +20,7 @@ class SessionController {
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(401).json({ error: 'User não encontrado' });
+      return res.status(401).json({ error: 'Usuário não encontrado' });
     }
 
     if (!(await user.checkPassword(password))) {
@@ -26,6 +28,7 @@ class SessionController {
     }
 
     const { id, name } = user;
+
     return res.json({
       user: {
         id,
@@ -33,9 +36,10 @@ class SessionController {
         email,
       },
       token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expireIn,
+        expiresIn: authConfig.expiresIn,
       }),
     });
   }
 }
+
 export default new SessionController();
