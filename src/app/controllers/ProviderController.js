2c88c862e5d3e08ca177a51008214b96515ec4d5
kaiorr <kaio.rocharibeiro@gmail.com>
2020-02-28 07:33:25 -0300

    Criação do controller de providers

---
+++ src/app/controllers/ProviderController.js
@@ -0,0 +1,14 @@
+import User from '../models/User';
+
+class ProviderController {
+  async index(req, res) {
+    const providers = await User.findAll({
+      where: { provider: true },
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+    });
+
+    return res.json(providers);
+  }
+}
+
+export default new ProviderController();
