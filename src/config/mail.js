30218d1a6901e1d673461aeff739b3d757364eb6
kaiorr <kaio.rocharibeiro@gmail.com>
2020-03-07 18:19:52 -0300

    add:(Config/Mail) Configuração do serviço de email utilizado para teste no MailTrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'af9c648e56673f',
+    pass: 'b7a92e6ed154e7',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com.br>',
+  },
+};
