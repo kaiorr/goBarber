58df6a3e5933157f0776d5828fff97b63a2f82ee
kaiorr <kaio.rocharibeiro@gmail.com>
2020-03-24 06:52:07 -0300

    Adicionando commitlint

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import routes from './routes';
+import './database/index';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+    this.server.use('/files',
+    express.static(path.resolve(__dirname, '..', 'temp', 'uploads')));
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9976a4f665cd8e8b35e74c4e57e808f6',
+  expireIn: '120d',
+};
